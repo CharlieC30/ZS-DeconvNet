675c1b6a260bc2e24564851219d327b37b4a9ad9
CharlieC30 <charliechangintw@gmail.com>
2025-11-17 11:47:53 +0800

    refactor: reorganize custom scripts and update paths
    Move custom training/inference scripts to custom_script/ directory.
    Rename train_custom_3d_gpu2.sh to train_custom_3d.sh and remove train_custom_3d_gpu1.sh.
    Update all data paths to new augmented_datasets structure.
    Replace hardcoded absolute paths with relative paths in shell scripts.
    Translate Chinese comments to English in all custom scripts.
    Update .gitignore to exclude augmented_datasets/ and trained_models/ (~51GB).

--- Python_MATLAB_Codes/train_inference_python/custom_script/train_custom_3d.sh
+++ Python_MATLAB_Codes/train_inference_python/custom_script/train_custom_3d.sh
@@ -1,11 +1,11 @@
-cd /home/aero/charliechang/projects/ZS-DeconvNet/Python_MATLAB_Codes/train_inference_python
+cd "$(dirname "$0")/.."
 
-source ~/anaconda3/etc/profile.d/conda.sh
+source "$(conda info --base)/etc/profile.d/conda.sh" 2>/dev/null || source ~/anaconda3/etc/profile.d/conda.sh
 conda activate zs-deconvnet
 
 python Train_ZSDeconvNet_3D.py \
   --psf_path '../../Python_PSF/PSFoutput/other/PSF_XY1.88um_Z15.04um_oddZ_118.tif' \
-  --data_dir '../your_augmented_datasets/iUExM/' \
+  --data_dir './augmented_datasets/augmented_datasets_3d/iUExM/' \
   --folder 'iUExM_roi_0916_1550_100' \
   --test_images_path '../../Raw_Data/FromGary/input/iUExM/roiC_crop128_1128.tif' \
   --save_weights_dir './my_models_3d' \
